85bf5ff57538f7244dff136fe8cbf4b8ebdd516b
Sudip Das <das366966@gmail.com>
2026-06-18 23:53:58 +0530

    refactor: migrate react-quill to react-quill-new to support React 19 compatibility

--- src/components/admin/RichTextEditor.tsx
+++ src/components/admin/RichTextEditor.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import 'react-quill/dist/quill.snow.css';
+import 'react-quill-new/dist/quill.snow.css';
 import { useMemo } from 'react';
 
-const ReactQuill = dynamic(() => import('react-quill'), { 
+const ReactQuill = dynamic(() => import('react-quill-new'), { 
   ssr: false,
   loading: () => <div className="h-64 flex items-center justify-center bg-[#111111] text-white/40 font-mono text-xs border border-white/5 rounded-xl">Loading Editor...</div>
 });
